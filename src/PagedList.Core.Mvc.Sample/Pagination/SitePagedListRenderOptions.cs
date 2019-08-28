d68f460a4b2cebae205aca8ecbaf0e791c38c449
Hieu Le <hieu.le@niteco.se>
2019-08-28 15:07:25 +0700

    Sample for customized rendering options

---
+++ src/PagedList.Core.Mvc.Sample/Pagination/SitePagedListRenderOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace PagedList.Core.Mvc.Sample.Pagination
+{
+    public class SitePagedListRenderOptions
+    {
+        public static PagedListRenderOptions Boostrap4
+        {
+            get
+            {
+                var option = PagedListRenderOptions.Bootstrap4Full;
+
+                option.MaximumPageNumbersToDisplay = 5;
+
+                return option;
+            }
+        }
+    }
+}
