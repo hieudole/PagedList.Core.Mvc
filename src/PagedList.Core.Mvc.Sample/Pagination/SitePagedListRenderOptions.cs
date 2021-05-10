5d3aef72e0889891cddb64e4a51e89718adba7a6
Hieu Le <hieudole@gmail.com>
2021-05-10 21:49:28 +0700

    Update sample to show how to customize the text of pager

--- src/PagedList.Core.Mvc.Sample/Pagination/SitePagedListRenderOptions.cs
+++ src/PagedList.Core.Mvc.Sample/Pagination/SitePagedListRenderOptions.cs
@@ -13,5 +13,21 @@
                 return option;
             }
         }
+
+        public static PagedListRenderOptions Boostrap4CustomizedText
+        {
+            get
+            {
+                var option = PagedListRenderOptions.Bootstrap4Full;
+
+                option.MaximumPageNumbersToDisplay = 5;
+                option.LinkToPreviousPageFormat = "<";
+                option.LinkToNextPageFormat = ">";
+                option.LinkToFirstPageFormat = "<<";
+                option.LinkToLastPageFormat = ">>";
+
+                return option;
+            }
+        }
     }
 }
